30be246fe50ae4d7bc03a00eec03798e7ff59cc6
vinaymaheshwari <vinaymaheshwari35@gmail.com>
2021-05-19 18:10:18 +0530

    Raise X To Power Y

---
+++ raise_x_to_power_y.java
@@ -0,0 +1,21 @@
+
+/**
+ * prac
+ */
+import java.util.Scanner;
+
+public class raise_x_to_power_y {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        sc.close();
+        int result = 1;
+        for (int i = 0; i < b; i++) {
+            result *= a;
+        }
+        System.out.println(result);
+    }
+
+}
