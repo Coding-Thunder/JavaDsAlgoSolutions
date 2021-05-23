c25700c3ac0e35aee53d887b1af1e12249da9cfe
vinaymaheshwari <vinaymaheshwari35@gmail.com>
2021-05-23 21:23:01 +0530

    finaly started to build algorithmic and data structures oriented thinking

--- prac.java
+++ prac.java
@@ -11,12 +11,12 @@ public class prac {
         System.out.println("Enter a range");
         int n = sc.nextInt();
         sc.close();
-        for (int j = 1; j <= n; j++) {
-            for (int i = 1; i <= j - 1; i++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i - 1; j++) {
                 System.out.print("  ");
             }
-            for (int i = 1; i <= n - j + 1; i++) {
-                System.out.print(i + " ");
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print("* ");
             }
             System.out.println();
         }
