5b503ed60382499b0e53e41b2cd0a768a5ddfaf5
vinaymaheshwari <vinaymaheshwari35@gmail.com>
2021-05-24 17:31:58 +0530

    Some Advanced Patterns To Strenthen The For Loops Concepts

--- prac.java
+++ prac.java
@@ -12,13 +12,11 @@ public class prac {
         int n = sc.nextInt();
         sc.close();
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i - 1; j++) {
-                System.out.print("  ");
-            }
             for (int j = 1; j <= n - i + 1; j++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
+
     }
 }
