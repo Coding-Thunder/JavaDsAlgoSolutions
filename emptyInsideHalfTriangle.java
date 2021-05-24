d9d5e2c99c1508d1928c851e90163d0ac45e77da
vinaymaheshwari <vinaymaheshwari35@gmail.com>
2021-05-24 19:28:42 +0530

    Practiced a lot Now Go To Array

--- emptyInsideHalfTriangle.java
+++ emptyInsideHalfTriangle.java
@@ -5,21 +5,18 @@ public class emptyInsideHalfTriangle {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         sc.close();
-        System.out.println("* ");
+        System.out.println("*");
         for (int i = 2; i <= n - 1; i++) {
-            System.out.print("* ");
+            System.out.print("*");
             for (int j = 1; j <= i - 2; j++) {
-                System.out.print("  ");
+                System.out.print(" ");
             }
             System.out.print("*");
             System.out.println();
-
         }
-        if (n > 1) {
-            for (int i = 1; i <= n; i++) {
-                System.out.print("* ");
-            }
-
+        for (int i = 1; i <= n; i++) {
+            System.out.print("*");
         }
+
     }
 }
--- prac.java
+++ prac.java
@@ -11,12 +11,17 @@ public class prac {
         System.out.println("Enter a range");
         int n = sc.nextInt();
         sc.close();
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n - i + 1; j++) {
-                System.out.print("* ");
+        System.out.println("*");
+        for (int i = 2; i <= n - 1; i++) {
+            System.out.print("*");
+            for (int j = 1; j <= i - 2; j++) {
+                System.out.print(" ");
             }
+            System.out.print("*");
             System.out.println();
         }
-
+        for (int i = 1; i <= n; i++) {
+            System.out.print("*");
+        }
     }
 }
