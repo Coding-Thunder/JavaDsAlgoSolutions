4e521e7779f38f170f3fa7213ef79afa0415bad0
vinaymaheshwari <vinaymaheshwari35@gmail.com>
2021-05-25 13:53:47 +0530

    These Problems are hard

---
+++ Assignment/pattern_Question_6.java
@@ -0,0 +1,28 @@
+package Assignment;
+
+/**
+ * pattern_Question_6
+ */
+import java.util.Scanner;
+
+public class pattern_Question_6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("  ");
+
+            }
+            if (i <= n)
+                System.out.print("1 ");
+            int number = 2;
+            for (int j = 1; j <= i - 1; j++) {
+                System.out.print(number + " " + (number + 1) + " ");
+            }
+            number = 2;
+            System.out.println();
+        }
+    }
+}
